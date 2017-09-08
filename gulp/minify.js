f0cb3ae1cc830ee310a481f4a44bc0fb2f1807af
Matthew J. Martin <matthew.mar10@gmail.com>
2017-09-08 10:39:20 +0800

    fix minification output filename

--- gulp/minify.js
+++ gulp/minify.js
@@ -4,6 +4,9 @@ const minify = require('gulp-minify');
 gulp.task('minify', function() {
   gulp.src('dist/*.js')
     .pipe(minify({
+      ext: {
+        min: '.min.js'
+      },
       ignoreFiles: ['.min.js']
     }))
     .pipe(gulp.dest('./dist/'))
